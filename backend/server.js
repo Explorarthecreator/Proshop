4c2446bd5e6887693482bee877b00faf64f96343
explorarthecreator <fredadejiemma99@gmail.com>
2022-03-11 23:09:40 +0100

    Installed dotenv and started working on ports and various variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 
 const app = express()
+
+
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req,res) =>{
     res.send('API is running')
@@ -14,4 +19,4 @@ app.get('/api/products/:id', (req,res) =>{
     const product = products.find((p) => p._id === req.params.id)
     res.send(product)
 })
-app.listen(5000, console.log("Server is running on port 5000"))
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`))
